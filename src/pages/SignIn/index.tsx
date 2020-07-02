c58b5a120d88015934f1e6ab01baef5ea974eb06
Ivan Seibel <ivanseibel@gmail.com>
2020-07-01 22:32:59 -0300

    Login validation

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useRef, useCallback } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import * as SC from './styles';
 import logoImg from '../../assets/logo.svg';
@@ -8,18 +13,39 @@ import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 const SignIn: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
+  // eslint-disable-next-line @typescript-eslint/ban-types
+  const handleSubmit = useCallback(async (data: object): Promise<void> => {
+    formRef.current?.setErrors({});
+    try {
+      const schema = Yup.object().shape({
+        email: Yup.string()
+          .required('Email is required')
+          .email('Must be a valid email'),
+        password: Yup.string().required('Password is required'),
+      });
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+    } catch (err) {
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors);
+    }
+  }, []);
+
   return (
     <SC.Container>
       <SC.Content>
         <img src={logoImg} alt="GoBarber" />
 
-        <form>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Log in to the app</h1>
 
           <Input name="email" icon={FiMail} placeholder="E-mail" />
 
           <Input
-            name="passord"
+            name="password"
             icon={FiLock}
             type="password"
             placeholder="Password"
@@ -28,7 +54,7 @@ const SignIn: React.FC = () => {
           <Button type="submit">Login</Button>
 
           <a href="forgot">I forgot my password</a>
-        </form>
+        </Form>
 
         <a href="login">
           <FiLogIn />
