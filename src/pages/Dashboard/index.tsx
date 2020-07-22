6c5225948faf965ca145c1823fff9986c057fe17
Ivan Seibel <ivanseibel@gmail.com>
2020-07-22 19:51:38 -0300

    Implement header to dashboard.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,31 @@
 import React from 'react';
 
+import { FiPower } from 'react-icons/fi';
 import * as SC from './styles';
+import logoImg from '../../assets/logo.svg';
+import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const { signOut, user } = useAuth();
+
   return (
     <SC.Container>
-      <h1>Dashboard</h1>
-      {/*
-       */}
+      <SC.Header>
+        <SC.HeaderContent>
+          <img src={logoImg} alt="GoBarber" />
+
+          <SC.Profile>
+            <img src={user.avatar_url} alt={user.name} />
+            <div>
+              <span>Welcome,</span>
+              <strong>{user.name}</strong>
+            </div>
+          </SC.Profile>
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </SC.HeaderContent>
+      </SC.Header>
     </SC.Container>
   );
 };
