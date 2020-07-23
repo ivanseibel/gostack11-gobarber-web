ddaf343c04fa4a7c8097d7f235cd19d202763f49
Ivan Seibel <ivanseibel@gmail.com>
2020-07-23 10:14:33 -0300

    Implement layout to show appointments list.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { FiPower, FiClock } from 'react-icons/fi';
 import * as SC from './styles';
@@ -6,6 +6,8 @@ import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
 
 const Dashboard: React.FC = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
   const { signOut, user } = useAuth();
 
   return (
@@ -49,7 +51,64 @@ const Dashboard: React.FC = () => {
               </span>
             </div>
           </SC.NextAppointment>
+
+          <SC.Section>
+            <strong>Morning</strong>
+
+            <SC.Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/42596775?s=460&u=8ddc06cf5793a75d7e7ad462ddfed52b8ef4d503&v=4"
+                  alt="Ivan Seibel"
+                />
+
+                <strong>Ivan Seibel</strong>
+              </div>
+            </SC.Appointment>
+
+            <SC.Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/42596775?s=460&u=8ddc06cf5793a75d7e7ad462ddfed52b8ef4d503&v=4"
+                  alt="Ivan Seibel"
+                />
+
+                <strong>Ivan Seibel</strong>
+              </div>
+            </SC.Appointment>
+          </SC.Section>
+
+          <SC.Section>
+            <strong>Afternoon</strong>
+
+            <SC.Appointment>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+
+              <div>
+                <img
+                  src="https://avatars0.githubusercontent.com/u/42596775?s=460&u=8ddc06cf5793a75d7e7ad462ddfed52b8ef4d503&v=4"
+                  alt="Ivan Seibel"
+                />
+
+                <strong>Ivan Seibel</strong>
+              </div>
+            </SC.Appointment>
+          </SC.Section>
         </SC.Schedule>
+        <SC.Calendar />
       </SC.Content>
     </SC.Container>
   );
