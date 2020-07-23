c9a9895d8785ec8f694ff4686def4b67aab03fdc
Ivan Seibel <ivanseibel@gmail.com>
2020-07-23 09:29:23 -0300

    Implement dashboard content.

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { FiPower } from 'react-icons/fi';
+import { FiPower, FiClock } from 'react-icons/fi';
 import * as SC from './styles';
 import logoImg from '../../assets/logo.svg';
 import { useAuth } from '../../hooks/auth';
@@ -26,6 +26,31 @@ const Dashboard: React.FC = () => {
           </button>
         </SC.HeaderContent>
       </SC.Header>
+
+      <SC.Content>
+        <SC.Schedule>
+          <h1>Schedule</h1>
+          <p>
+            <span>Today</span>
+            <span>Day 06</span>
+            <span>Monday</span>
+          </p>
+          <SC.NextAppointment>
+            <strong>Next service</strong>
+            <div>
+              <img
+                src="https://avatars0.githubusercontent.com/u/42596775?s=460&u=8ddc06cf5793a75d7e7ad462ddfed52b8ef4d503&v=4"
+                alt="Ivan Seibel"
+              />
+              <strong>Ivan Seibel</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </SC.NextAppointment>
+        </SC.Schedule>
+      </SC.Content>
     </SC.Container>
   );
 };
