6a495cf28767a9e29ca3c8e1eb376e4c220d4ee4
Ivan Seibel <ivanseibel@gmail.com>
2020-07-29 16:17:45 -0300

    Implement login test.

--- src/__tests__/pages/SignIn.spec.tsx
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -1,17 +1,30 @@
 import React, { ReactNode } from 'react';
 import { render } from '@testing-library/react';
+import { fireEvent } from '@testing-library/dom';
 import SignIn from '../../pages/SignIn';
+
+const mockedHistoryPush = jest.fn();
 
 jest.mock('react-router-dom', () => {
   return {
-    useHistory: jest.fn(),
+    useHistory: () => ({ push: mockedHistoryPush }),
     Link: ({ children }: { children: ReactNode }) => children,
   };
 });
 
 describe('SignIn page', () => {
   it('should be able to sign in', () => {
-    const { debug } = render(<SignIn />);
-    debug();
+    const { getByPlaceholderText, getByText } = render(<SignIn />);
+
+    const emailField = getByPlaceholderText('E-mail');
+    const passwordField = getByPlaceholderText('Password');
+    const buttonElement = getByText('Logon');
+
+    fireEvent.change(emailField, { target: { value: 'johndoe@email.com' } });
+    fireEvent.change(passwordField, { target: { value: '123456' } });
+
+    fireEvent.click(buttonElement);
+
+    expect(mockedHistoryPush).toHaveBeenCalledWith('/dashboard');
   });
 });
