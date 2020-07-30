244437fc2e82cdd3898f6942171f696407cab86d
Ivan Seibel <ivanseibel@gmail.com>
2020-07-30 09:17:58 -0300

    Create api mock.

---
+++ src/__tests__/hooks/auth.spec.tsx
@@ -0,0 +1,46 @@
+import { renderHook } from '@testing-library/react-hooks';
+import MockAdapter from 'axios-mock-adapter';
+import { useAuth, AuthProvider } from '../../hooks/auth';
+import api from '../../services/api';
+
+const apiMock = new MockAdapter(api);
+
+const apiResponse = {
+  user: {
+    id: 'user-id',
+    name: 'John Doe',
+    email: 'johndoe@email.com',
+    avatar_url: 'https://api.adorable.io/avatars/100/John Doe.png',
+  },
+  token: 'user-token',
+};
+
+describe('Auth Hook', () => {
+  it('should be able to sign in', async () => {
+    apiMock.onPost('sessions').reply(200, apiResponse);
+
+    const setItemSpy = jest.spyOn(Storage.prototype, 'setItem');
+
+    const { result, waitForNextUpdate } = renderHook(() => useAuth(), {
+      wrapper: AuthProvider,
+    });
+
+    result.current.signIn({
+      email: 'johndoe@email.com',
+      password: '123456',
+    });
+
+    await waitForNextUpdate();
+
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:token',
+      apiResponse.token,
+    );
+    expect(setItemSpy).toHaveBeenCalledWith(
+      '@GoBarber:user',
+      JSON.stringify(apiResponse.user),
+    );
+
+    expect(result.current.user.email).toBe('johndoe@email.com');
+  });
+});
